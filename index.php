c1e827e1c344c074987f7b27448adfe8d368f0a8
Ayron-Kevin <128102260+Ayron-Kevin@users.noreply.github.com>
2023-06-07 21:21:29 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-olá
-
-
+<?php
+session_start();
+?>
+<html>
+<body>
+<center> 
+    olá, <?php
+    if(isset($_SESSION['nome'])==null){
+        ?>visitante
+        <a href="login.php">login</a>
+        <?php  } else {
+            echo $_SESSION['nome']; ?>
+        <a href="cadastrar.php">cadastrar</a><br>
+        <a href= "logout.php">sair</a>
+         <?php } ?>
+<body>
+<html>
